dc64f630de45119d6e04521f050aa5ec5f43d77e
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-02-11 16:03:17 +1100

    initialize audio services

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,10 +1,14 @@
 import 'package:test_audio/logic/page_manager.dart';
 import 'playlist_repository.dart';
 import 'package:get_it/get_it.dart';
+import 'package:audio_service/audio_service.dart';
+import 'audio_handler.dart';
 
 GetIt getIt = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
+
+  getIt.registerSingleton<AudioHandler>(await initAudioService());
   // services
   getIt.registerLazySingleton<PlaylistRepository>(() => DemoPlaylist());
 
