458bfdf3383ca60eb2c74c7c617861133b8060cf
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-02-11 16:01:55 +1100

    create a audio handler

--- lib/services/audio_handler.dart
+++ lib/services/audio_handler.dart
@@ -1,10 +1,7 @@
 import 'package:audio_service/audio_service.dart';
-import 'package:just_audio/just_audio.dart';
-
-
 Future<AudioHandler> initAudioService() async {
   return await AudioService.init(
-    builder: () => AudioPlayerHandler(),
+    builder: () => MyAudioHandler(),
     config: AudioServiceConfig(
       androidNotificationChannelId: 'com.mycompany.myapp.audio',
       androidNotificationChannelName: 'Audio Service Demo',
@@ -13,18 +10,6 @@ Future<AudioHandler> initAudioService() async {
     ),
   );
 }
-
-
-class AudioPlayerHandler extends BaseAudioHandler {
-  final _player = AudioPlayer();
-
-  AudioPlayerHandler() {
-    _player.setUrl("https://live.sgpc.net:8443/;nocache=889869audio_file.mp3");
-  }
-
-  @override
-  Future<void> play() => _player.play();
-
-  @override
-  Future<void> pause() => _player.pause();
+class MyAudioHandler extends BaseAudioHandler {
+  // TODO: Override needed methods
 }
